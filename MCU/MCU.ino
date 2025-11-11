a4b4b9e6cec690afb77e3963d66be831b6a5d0e8
TheerawutP <theerawut.p@flinkone.com>
2025-11-11 11:13:15 +0700

    add tasks

--- MCU/MCU.ino
+++ MCU/MCU.ino
@@ -1,7 +1,50 @@
-void setup(){
+//include libs
+#include "RCSwitch.h"
+#include "queue.h"
 
+//define constants
+//define macros
+//define rtos handles
+RCSwitch mySwitch = RCSwitch();
+
+void vTransit(void *arg){
+  for(;;){
+
+  }
 }
 
-void loop(){
+void vGetPosition(void *arg){
+  for(;;){
 
+  }
+}
+
+
+void vRecieve(void *arg){
+  for(;;){
+    if (mySwitch.available()) {
+      int cmd = mySwitch.getReceivedValue();
+      switch (cmd) {
+
+        case to_1:
+        xQueueSend(xQueueTransit,,);
+          break;
+
+        case to_2:
+          break;
+        case to_3:
+          break;
+        case IN_USED:
+          break;
+      }
+  }
+} 
+
+void setup() {
+  Serial.begin(115200);
+  mySwitch.enableReceive(22); //attach interrupt to 22
+}
+
+
+void loop() {
 }
