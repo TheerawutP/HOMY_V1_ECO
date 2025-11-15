191f3d530ca40e8be04939749bbbea9a563db418
TheerawutP <theerawut.p@flinkone.com>
2025-11-15 15:49:35 +0700

    add SPIFFS, add vStatusLogger for logging lift

--- MCU/MCU.ino
+++ MCU/MCU.ino
@@ -1,5 +1,7 @@
 //include libs
 #include "RCSwitch.h"
+#include "FS.h"
+#include "SPIFFS.h"
 
 //define instances
 #define RFReceiver 22
@@ -17,26 +19,74 @@
 #define toFloor2 174740
 #define toFloor3 174738
 
+#define DEBOUNCE_MS 200
+#define BRAKE_MS 1500
+
+#define FORMAT_SPIFFS_IF_FAILED true
+
+//define enum
+enum direction_t {
+  UP,
+  DOWN
+};
+
+// enum state_t {
+//   IDLE,
+//   MOVING_UP,
+//   MOVING_DOWN,
+// };
+
 //define macros
-#define BRK_ON digitalWrite(BRK, HIGH); Serial.println("Brake ON");
-#define BRK_OFF digitalWrite(BRK, LOW); Serial.println("Brake OFF");
-#define M_RUN digitalWrite(EN, LOW); Serial.println("Motor Run");
-#define M_STP digitalWrite(EN, HIGH); Serial.println("Motro Stop");
-#define M_UP digitalWrite(FR, HIGH); Serial.println("Move UP");
-#define M_DW digitalWrite(FR, LOW); Serial.println("Move Down");
 
-#define ROTATE(dir) \
-  do { \
-    if (dir == UP) { \
-      M_UP; \
-    } else if (dir == DOWN) { \
-      M_DW; \
-    } \
-    BRK_OFF; \
-    M_RUN; \
-  } while (0)
+//inline functions
+inline void ROTATE(direction_t dir) {
+    if (dir == UP) {
+        digitalWrite(FR, HIGH);
+        Serial.println("Move UP");
+    } 
+    else if (dir == DOWN) {
+        digitalWrite(FR, LOW);
+        Serial.println("Move DOWN");
+    }
+    
+    digitalWrite(EN, LOW);
+    Serial.println("Motor Run");
+}
 
-#define BrkState digitalRead(BRK)
+inline void BRK_ON() {
+    digitalWrite(BRK, HIGH);
+    Serial.println("Brake ON");
+}
+
+inline void BRK_OFF() {
+    digitalWrite(BRK, LOW);
+    Serial.println("Brake OFF");
+}
+
+inline void M_RUN() {
+    digitalWrite(EN, LOW);
+    Serial.println("Motor Run");
+}
+
+inline void M_STP() {
+    digitalWrite(EN, HIGH);
+    Serial.println("Motor Stop");
+}
+
+inline void M_UP() {
+    digitalWrite(FR, HIGH);
+    Serial.println("Move UP");
+}
+
+inline void M_DW() {
+    digitalWrite(FR, LOW);
+    Serial.println("Move Down");
+}
+
+inline int BrkState() {
+    return digitalRead(BRK);
+}
+
 //define rtos handles
 QueueHandle_t xQueueTransit;
 QueueHandle_t xQueueGetDirection;
@@ -45,41 +95,130 @@ SemaphoreHandle_t xSemDoneTransit = NULL;
 SemaphoreHandle_t xSemLanding;
 TimerHandle_t xDisbrakeTimer;
 TaskHandle_t xLandingHandle;
-
-//define enum
-enum direction_t {
-  UP,
-  DOWN
-};
+SemaphoreHandle_t xTransitMutex;
 
 //define structs and objs
 typedef struct {
   uint8_t floor;
   direction_t dir;
-} TRANSIT;
+}TRANSIT;
 
 TRANSIT transit;
 RCSwitch RF = RCSwitch();
 
 //helper functions
 //define global
-volatile uint8_t POS = 0;
+volatile uint8_t POS = -1;
+volatile uint8_t defaultPOS = 1;
 volatile uint8_t TARGET = -1;
-volatile unsigned long lastFloorISR = 0;
+volatile unsigned long lastFloorISR_1 = 0;
+volatile unsigned long lastFloorISR_2 = 0;
+volatile unsigned long lastFloorISR_3 = 0;
 volatile unsigned long lastNoPowerISR = 0;
 volatile unsigned long lastResetSysISR = 0;
 bool emergency = false;
 
-/*
-freeRTOS task explanaton
+//tasks prototypes
+void vReceive(void *arg);
+void vDisbrake(TimerHandle_t xDisbrake);
+void vTransit(void *arg);
+void vGetDirection(void *arg);
+void vStopper(void *arg);
+void vLanding(void *arg);
+void vDisbrake(TimerHandle_t xDisbrake);
+void ISR_atFloor1();
+void ISR_atFloor2();
+void ISR_atFloor3();
+void ISR_Landing();
+void ISR_ResetSystem();
+void vStatusLog(void *arg);
+//file system helper
+bool fileExists(fs::FS &fs, const char *path);
+String readFileAsString(fs::FS &fs, const char *path);
+int readFileAsInt(fs::FS &fs, const char *path);
+void writeFile(fs::FS &fs, const char *path, int value);
+void writeFile(fs::FS &fs, const char *path, const char *message);
 
-*/
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  }
+
+  if (SPIFFS.exists("/current_pos.txt")) {
+    POS = readFileAsInt(SPIFFS, "/current_pos.txt");  
+  } else {
+    POS = defaultPOS;  
+    writeFile(SPIFFS, "/current_pos.txt", POS); 
+  }
+  RF.enableReceive(RFReceiver);  //attach interrupt to 22
+
+  //pos sensor
+  pinMode(EN, OUTPUT);
+  pinMode(FR, OUTPUT);
+
+  pinMode(floorSensor1, INPUT_PULLUP);
+  pinMode(floorSensor2, INPUT_PULLUP);
+  pinMode(floorSensor3, INPUT_PULLUP);
+  attachInterrupt(floorSensor1, ISR_atFloor1, FALLING);
+  attachInterrupt(floorSensor2, ISR_atFloor2, FALLING);
+  attachInterrupt(floorSensor3, ISR_atFloor3, FALLING);
+  
+  pinMode(BRK, OUTPUT);
+  pinMode(NP, INPUT_PULLUP);
+  attachInterrupt(NP, ISR_Landing, FALLING);
+
+  pinMode(CS, OUTPUT);
+  digitalWrite(CS, HIGH); //wake receiver up
+
+  pinMode(RST_SYS, INPUT_PULLUP);
+  attachInterrupt(RST_SYS, ISR_ResetSystem, FALLING);
+
+  BRK_ON;
+  xSemTransit = xSemaphoreCreateBinary();
+  xSemDoneTransit = xSemaphoreCreateBinary();
+  xSemLanding = xSemaphoreCreateBinary();
+  xTransitMutex = xSemaphoreCreateMutex();
+  xQueueGetDirection = xQueueCreate(1, sizeof(uint8_t));
+  xDisbrakeTimer = xTimerCreate("DisbrakeTimer", BRAKE_MS, pdFALSE, NULL, vDisbrake);
+  xTaskCreate(vReceive, "Receive", 1024, NULL, 2, NULL);
+  xTaskCreate(vGetDirection, "GetDirection", 1024, NULL, 3, NULL);
+  xTaskCreate(vTransit, "Transit", 1024, NULL, 3, NULL);
+  xTaskCreate(vStopper, "Stopper", 1024, NULL, 4, NULL);
+  xTaskCreate(vLanding, "Landing", 2048, NULL, 4, &xLandingHandle);
+  xTaskCreate(vStatusLogger, "StatusLogger", 2048, NULL, 2, NULL);
+}
+
+void loop() {
+  // if (RF.available()) {
+  //   Serial.println(RF.getReceivedValue());
+  //   Serial.println(RF.getReceivedBitlength());
+  //   Serial.println(RF.getReceivedDelay());
+  //   Serial.println(RF.getReceivedProtocol());
+  //   RF.resetAvailable();
+  // }
+  // vTaskDelay(1000);
+  int curr_pos = readFileAsInt(SPIFFS, "/current_pos.txt");
+  Serial.print("curr_pos in fs: "); Serial.println(curr_pos);
+  vTaskDelay(5000);
+}
+
+
+
+//freeRTOS task bodies
 void vTransit(void *arg) {
   for (;;) {
     if (xSemaphoreTake(xSemTransit, portMAX_DELAY) == pdTRUE) {
-      TARGET = transit.floor;
-      Serial.println("start transit");
-      ROTATE(transit.dir);
+      if(xSemaphoreTake(xTransitMutex, portMAX_DELAY) == pdTRUE){
+        TARGET = transit.floor;
+        xSemaphoreGive(xTransitMutex);
+      }
+        BRK_OFF();
+        Serial.println("start transit");
+        ROTATE(transit.dir);
     }
   }
 }
@@ -90,13 +229,19 @@ void vGetDirection(void *arg) {
   for (;;) {
     if (xQueueReceive(xQueueGetDirection, &target, portMAX_DELAY) == pdTRUE) {
       if(POS != target){
-      dir = (target > POS) ? UP : DOWN;
-      transit.floor = target;
-      transit.dir = dir;
-      xSemaphoreGive(xSemTransit);
+        dir = (target > POS) ? UP : DOWN;
+
+        if(xSemaphoreTake(xTransitMutex, portMAX_DELAY) == pdTRUE){
+          transit.floor = target;
+          transit.dir = dir;
+          xSemaphoreGive(xTransitMutex);
+        }
+
+        xSemaphoreGive(xSemTransit);
       }else{
-        Serial.println("We are reached");
+        Serial.println("It's here");
       }
+
     }
   }
 }
@@ -104,8 +249,9 @@ void vGetDirection(void *arg) {
 void vStopper(void *arg) {
   for(;;){
   if (xSemaphoreTake(xSemDoneTransit, portMAX_DELAY) == pdTRUE) {
-    M_STP;
-    BRK_ON;
+    M_STP();
+    BRK_ON();
+    TARGET = 0;
     }
   }
 }
@@ -116,28 +262,28 @@ void vLanding(void *arg) {
 
      if (POS == 1 && emergency == true) {
         Serial.println("finish Safety landing");
-        M_STP;   
-        BRK_OFF;  
+        M_STP();   
+        BRK_OFF();  
         emergency = false;  
         xTimerStop(xDisbrakeTimer, 0);
         vTaskSuspend(NULL); 
     }
-    BRK_ON;
+    BRK_ON();
     xTimerStart(xDisbrakeTimer, 0);
     }
   }
 }
 
 void vDisbrake(TimerHandle_t xDisbrake) {
-  BRK_OFF;
+  BRK_OFF();
   delay(200);
   xSemaphoreGive(xSemLanding);
 }
 
 void ISR_atFloor1() {
   unsigned long now = millis();
-  if (now - lastFloorISR < 200) return;  // debounce 50ms
-  lastFloorISR = now;
+  if (now - lastFloorISR_1 < DEBOUNCE_MS) return;  // debounce 50ms
+  lastFloorISR_1 = now;
   Serial.println("reach floor1");
 
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
@@ -151,8 +297,8 @@ void ISR_atFloor1() {
 
 void ISR_atFloor2() {
   unsigned long now = millis();
-  if (now - lastFloorISR < 200) return;  
-  lastFloorISR = now;
+  if (now - lastFloorISR_2 < DEBOUNCE_MS) return;  
+  lastFloorISR_2 = now;
   Serial.println("reach floor2");
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
   POS = 2;
@@ -165,8 +311,8 @@ void ISR_atFloor2() {
 
 void ISR_atFloor3() {
   unsigned long now = millis();
-  if (now - lastFloorISR < 200) return;  
-  lastFloorISR = now;
+  if (now - lastFloorISR_3 < DEBOUNCE_MS) return;  
+  lastFloorISR_3 = now;
   Serial.println("reach floor3");
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
   POS = 3;
@@ -179,19 +325,19 @@ void ISR_atFloor3() {
 
 void ISR_Landing() {
   unsigned long now = millis();
-  if (now - lastFloorISR < 200) return;  
+  if (now - lastNoPowerISR < DEBOUNCE_MS) return;  
   lastNoPowerISR = now;
   Serial.println("NO POWER is detected!");
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
   emergency = true;
-  vTaskResume(xLandingHandle);
+  xTaskResumeFromISR(xLandingHandle);
   xSemaphoreGiveFromISR(xSemLanding, &xHigherPriorityTaskWoken);
   portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 void ISR_ResetSystem() {
   unsigned long now = millis();
-  if (now - lastResetSysISR < 200) return;  
+  if (now - lastResetSysISR < DEBOUNCE_MS) return;  
   lastResetSysISR = now;
   Serial.println("Reset System, Back to Floor1");
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
@@ -234,55 +380,59 @@ void vReceive(void *arg) {
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  RF.enableReceive(RFReceiver);  //attach interrupt to 22
-
-  //pos sensor
-  pinMode(EN, OUTPUT);
-  pinMode(FR, OUTPUT);
-
-  pinMode(floorSensor1, INPUT_PULLUP);
-  pinMode(floorSensor2, INPUT_PULLUP);
-  pinMode(floorSensor3, INPUT_PULLUP);
-  attachInterrupt(floorSensor1, ISR_atFloor1, FALLING);
-  attachInterrupt(floorSensor2, ISR_atFloor2, FALLING);
-  attachInterrupt(floorSensor3, ISR_atFloor3, FALLING);
-  
-  pinMode(BRK, OUTPUT);
-  pinMode(NP, INPUT_PULLUP);
-  attachInterrupt(NP, ISR_Landing, FALLING);
-
-  pinMode(CS, OUTPUT);
-  digitalWrite(CS, HIGH); //wake receiver up
-
-  pinMode(RST_SYS, INPUT_PULLUP);
-  attachInterrupt(RST_SYS, ISR_ResetSystem, FALLING);
-
-  BRK_ON;
-  xSemTransit = xSemaphoreCreateBinary();
-  xSemDoneTransit = xSemaphoreCreateBinary();
-  xSemLanding = xSemaphoreCreateBinary();
-  //xQueueTransit = xQueueCreate(10, sizeof( transit ) );
-  xQueueGetDirection = xQueueCreate(1, sizeof(uint8_t));
-  xDisbrakeTimer = xTimerCreate("DisbrakeTimer", 1500, pdTRUE, NULL, vDisbrake);
-  xTaskCreate(vReceive, "Receive", 1024, NULL, 2, NULL);
-  xTaskCreate(vGetDirection, "GetDirection", 1024, NULL, 2, NULL);
-  xTaskCreate(vTransit, "Transit", 1024, NULL, 2, NULL);
-  xTaskCreate(vStopper, "Stopper", 1024, NULL, 2, NULL);
-  xTaskCreate(vLanding, "Landing", 2048, NULL, 2, &xLandingHandle);
+void vStatusLogger(void *arg) {
+  int lastPOS = -1;
+  for(;;){
+    if(POS != lastPOS){
+      writeFile(SPIFFS, "/current_pos.txt", POS);
+      lastPOS = POS;
+    }
+    vTaskDelay(1000);
+  } 
 }
 
-void loop() {
-  // if (RF.available()) {
-  //   Serial.println(RF.getReceivedValue());
-  //   Serial.println(RF.getReceivedBitlength());
-  //   Serial.println(RF.getReceivedDelay());
-  //   Serial.println(RF.getReceivedProtocol());
-  //   RF.resetAvailable();
-  // }
-  // vTaskDelay(1000);
-
-  //Serial.println(POS);
-  //vTaskDelay(5000);
+bool fileExists(fs::FS &fs, const char *path) {
+  return fs.exists(path);
 }
+
+String readFileAsString(fs::FS &fs, const char *path) {
+  File file = fs.open(path, FILE_READ);
+  if (!file || file.isDirectory()) {
+    Serial.printf("Failed to open file %s for reading\n", path);
+    return "";
+  }
+
+  String content = "";
+  while (file.available()) {
+    content += char(file.read()); 
+  }
+  file.close();
+  return content;
+}
+
+int readFileAsInt(fs::FS &fs, const char *path) {
+  String str = readFileAsString(fs, path);
+  str.trim(); 
+  return str.toInt();
+}
+
+void writeFile(fs::FS &fs, const char *path, int value) {
+  writeFile(fs, path, String(value).c_str());
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Writing file: %s\r\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("- file written");
+  } else {
+    Serial.println("- write failed");
+  }
+  file.close();
+}
+
